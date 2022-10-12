f4555823b89fc5538ff7613e1848aef6a75abd54
arakaki <takehiroarakaki@gmail.com>
2022-10-12 15:13:30 +0900

    ・データ範囲60日間に変更 ・ドメイン形式をフルパスに変更

--- popup.ts
+++ popup.ts
@@ -8,52 +8,46 @@ document.addEventListener('DOMContentLoaded', () => {
   divs[0] = divs[0].addEventListener('click', postClickEvent)
 })
 
-let accessData;
+const accessArray = []
 
 // SEARCHクリックイベント
 const searchClickEvent = () =>  {
   const ul = document.querySelector('#historyList')
   const searchText = document.querySelector('#searchText')
-  const microsecondsPerDay = 1000 * 60 * 60 * 24
-  // とりあえず過去20年を対象
-  const searchStartDate = (new Date).getTime() - microsecondsPerDay * 365 * 20
 
-  // TODO:startTime, endTimeをテキストボックスから指定
+  // 60日間(2ヶ月前)
+  const microsecondsPerDay = 1000 * 60 * 60 * 24
+  const searchStartDate = (new Date).getTime() - microsecondsPerDay * 60
+
   const searchQuery = {
     text: searchText.value,
     startTime: searchStartDate,
-    maxResults: 10000
+    maxResults: 100000
   }
-
-  const accessArray = []
-  const regDomain = /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/img
 
   // 履歴取得
   chrome.history.search(searchQuery, function (accessItems) {
     // 履歴の数だけループし、検索結果を表示する
     for (let i = 0; i < accessItems.length; i++) {
-      const domain = accessItems[i].url.match(regDomain)
-      accessArray.push(domain)
+      const accessObj = {};
+      accessObj[accessItems[i].url] = accessItems[i].lastVisitTime;
+
+      const accessData = JSON.stringify(accessObj)
+      accessArray.push(accessData)
     }
 
-    let accessObj = {};
-    for (let i = 0; i < accessArray.length; i++) {
-      const domain = accessArray[i];
-      accessObj[domain] = (accessObj[domain] || 0) + 1;
-    }
-    accessData = JSON.stringify(Object.entries(accessObj).map(([key, value]) => ({[key]: value})))
-    ul.innerHTML = `target: ${accessData}`
+    ul.innerHTML = `target: ${accessArray}`
   })
 }
 
 // POSTクリックイベント
 const postClickEvent = () =>  {
   const url = 'http://localhost:3000'
-  // テスト時はここのサブディレクトリを変更
+  // ここのサブディレクトリを変更でローカルで確認可
   const subdir = '/v1/hoge'
   const xhr = new XMLHttpRequest();
 
   xhr.open('POST', `${url}${subdir}`);
   xhr.setRequestHeader('content-type', 'application/x-www-form-urlencoded;charset=UTF-8');
-  xhr.send(`urls=${accessData}`);
+  xhr.send(`urls=${accessArray}`);
 }
