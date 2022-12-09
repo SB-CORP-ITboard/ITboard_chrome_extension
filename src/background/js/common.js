1a909820ebe699a3d6625e1be14918130e35e332
arakaki <takehiroarakaki@gmail.com>
2022-12-09 09:12:07 +0900

    リクエスト例外処理

--- src/background/js/common.js
+++ src/background/js/common.js
@@ -2,99 +2,103 @@ import { con } from "./const.js";
 
 // ユーザーのリクエストの順序を取得
 export const postBatchDataEvent = async (email) => {
-  const response = await fetch(con.postDistributeUrl, {
-    headers:{
-      'Accept': 'application/json, */*',
-      'Content-type':'application/json'
-    },
-    method: "POST",
-    body: JSON.stringify({ email: email }),
-  });
-
-  if(response.ok){
-    const data = await response.json();
-    // 分割(distributeHour)
-    //  1の場合: calcDistribute == 60分
-    //  2の場合: calcDistribute == 120分
-    const calcDistribute = data.distributeHour * 60;
-    let requestIndex = data.requestIndex;
-
-    // ９時に履歴取得開始
-    // calcDistributeの値に応じてユーザーのリクエスト時間を配分
-    // case1
-    //   requestIndex: 10, calcDistribute: 60
-    //    └ 9:10にリクエスト
-    // case2
-    //   requestIndex: 90, calcDistribute: 60
-    //    └ 9:30にリクエスト
-    // case3
-    //   requestIndex: 90, calcDistribute: 120
-    //    └ 10:30にリクエスト
-    // case4
-    //   requestIndex: 280, calcDistribute: 120
-    //    └ 9:40にリクエスト
-
-    // requestIndexがcalcDistributeより少ない場合 (case1とcase3)
-    // そのままrequestIndexを保存
-    let num = requestIndex / calcDistribute;
-    if (num < 1) {
-      chrome.storage.local.set({ requestIndex: requestIndex });
-      return requestIndex;
-    }
-
-    // requestIndexがcalcDistributeより多い場合 (case2とcase4)
-    // requestIndex値を減算し、calcDistributeとの徐算結果が1以下の場合に保存
-    // 下記case4の場合
-    //  1回目の処理:
-    //   └ 280 - 120 = 160
-    //   └ 160 / 120 = 1.3
-    //  2回目の処理:
-    //   └ 160 - 120 = 40
-    //   └ 40 / 120 = 0.3 → 40を保存
-    while (num < 1) {
-      requestIndex -= calcDistribute;
-      num = requestIndex / calcDistribute;
-      if (num < 1) {
-        chrome.storage.local.set({ requestIndex: requestIndex });
-        return requestIndex;
-      }
-    }
-  };
-};
-
-// ブラウザ履歴取得
-export const historyEvent = (email) => {
-  const browser = historyByBrowser();
-
-  const accessArray = []
-  chrome.history.search(con.searchQuery, (accessItems) => {
-    for (let i = 0; i < accessItems.length; i++) {
-      const accessObj = {};
-
-      // クエリパラメータ除外
-      accessObj["url"] = accessItems[i].url.replace(/\?.*$/, "");
-      accessObj["title"] = accessItems[i].title;
-      accessObj["accessCount"] = accessItems[i].visitCount;
-      accessObj["lastAccessDate"] = new Date(accessItems[i].lastVisitTime);
-
-      const accessData = accessObj;
-      accessArray.push(accessData);
-    }
-
-    // 履歴データPOST
-    fetch(con.postShadowItUrl, {
+  try {
+    const response = await fetch(con.postDistributeUrl, {
       headers:{
         'Accept': 'application/json, */*',
         'Content-type':'application/json'
       },
       method: "POST",
-      body: JSON.stringify({
-        email: email,
-        browser: browser,
-        data: accessArray
-      }),
+      body: JSON.stringify({ email: email }),
     });
-  });
+
+    if (response.ok) {
+      const data = await response.json();
+
+      // 分割(distributeHour)
+      //  1の場合: calcDistribute == 60分
+      //  2の場合: calcDistribute == 120分
+      const calcDistribute = data.distributeHour * 60;
+      let requestIndex = data.requestIndex;
+
+      // ９時に履歴取得開始
+      // calcDistributeの値に応じてユーザーのリクエスト時間を配分
+      // case1
+      //   requestIndex: 10, calcDistribute: 60
+      //    └ 9:10にリクエスト
+      // case2
+      //   requestIndex: 90, calcDistribute: 60
+      //    └ 9:30にリクエスト
+      // case3
+      //   requestIndex: 90, calcDistribute: 120
+      //    └ 10:30にリクエスト
+      // case4
+      //   requestIndex: 280, calcDistribute: 120
+      //    └ 9:40にリクエスト
+
+      // requestIndexがcalcDistributeより少ない場合 (case1とcase3)
+      // そのままrequestIndexを保存
+      let num = requestIndex / calcDistribute;
+      if (num < 1) {
+        chrome.storage.local.set({ requestIndex: requestIndex });
+        return requestIndex;
+      }
+
+      // requestIndexがcalcDistributeより多い場合 (case2とcase4)
+      // requestIndex値を減算し、calcDistributeとの徐算結果が1以下の場合に保存
+      // 下記case4の場合
+      //  1回目の処理:
+      //   └ 280 - 120 = 160
+      //   └ 160 / 120 = 1.3
+      //  2回目の処理:
+      //   └ 160 - 120 = 40
+      //   └ 40 / 120 = 0.3 → 40を保存
+      while (num < 1) {
+        requestIndex -= calcDistribute;
+        num = requestIndex / calcDistribute;
+        if (num < 1) {
+          chrome.storage.local.set({ requestIndex: requestIndex });
+          return requestIndex;
+        }
+      }
+    };
+  } catch(e) { console.log(`${e} from postBatchDataEvent `) }
+};
+
+// ブラウザ履歴取得
+export const historyEvent = (email) => {
+  try {
+    const browser = historyByBrowser();
+
+    const accessArray = []
+    chrome.history.search(con.searchQuery, (accessItems) => {
+      for (let i = 0; i < accessItems.length; i++) {
+        const accessObj = {};
+
+        // クエリパラメータ除外
+        accessObj["url"] = accessItems[i].url.replace(/\?.*$/, "");
+        accessObj["title"] = accessItems[i].title;
+        accessObj["accessCount"] = accessItems[i].visitCount;
+        accessObj["lastAccessDate"] = new Date(accessItems[i].lastVisitTime);
+
+        const accessData = accessObj;
+        accessArray.push(accessData);
+      }
+    // 履歴データPOST
+      fetch(con.postShadowItUrl, {
+        headers:{
+          'Accept': 'application/json, */*',
+          'Content-type':'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify({
+          email: email,
+          browser: browser,
+          data: accessArray
+        }),
+      });
+    });
+  } catch(e) { console.log(`${e} from historyEvent `) }
 };
 
 // 履歴取得したブラウザを判別
